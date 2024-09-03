b0b93766a2ce06a949cdd4276ea2fce819b11fd9
rhuwdas <118279137+rhuwdas@users.noreply.github.com>
2024-09-03 15:14:47 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <section className="Foother_cal">
+        <label htmlFor=""></label>
+        <input type="text" />
+        <input type="button" value="" />
+        <select name="" id="">
+          <option value=""></option>
+        </select>
+      </section>
+    </main>
+  )
+}
+
+export default App
